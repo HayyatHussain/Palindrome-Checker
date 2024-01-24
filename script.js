52a6a55868ec501ca8fd3829800e19571bdc5a45
HayyatHussain <145599914+HayyatHussain@users.noreply.github.com>
2024-01-24 11:35:27 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+const userInput = document.getElementById("text-input");
+const button = document.getElementById("check-btn");
+const output = document.getElementById("result");
+
+
+const palindrome = () => {
+  const regex = /[^a-zA-Z0-9]/g;
+  const filteredInput = userInput.value.replace(regex, "");
+  const lowerCaseStr = filteredInput.toLowerCase();
+  const reverseStr = lowerCaseStr.split("").reverse().join("");
+  if (lowerCaseStr.length == 0) {
+    alert("Please input a value");
+    return;
+  } else if (lowerCaseStr == reverseStr) {
+      output.innerText = `${userInput.value} is a palindrome`;
+  } else {
+    output.innerText = `${userInput.value} is not a palindrome`;
+  }
+}
+
+
+button.addEventListener("click", () => {
+  palindrome();
+  userInput.value = "";
+});
