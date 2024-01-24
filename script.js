800c1c74e9e1eb8e48fc7a50f74c538df9d320b6
HayyatHussain <145599914+HayyatHussain@users.noreply.github.com>
2024-01-24 11:42:47 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+// Get the DOM elements
 const userInput = document.getElementById("text-input");
 const button = document.getElementById("check-btn");
 const output = document.getElementById("result");
 
-
+// Function to check if the user input is a palindrome
 const palindrome = () => {
   const regex = /[^a-zA-Z0-9]/g;
   const filteredInput = userInput.value.replace(regex, "");
@@ -18,11 +19,13 @@ const palindrome = () => {
   }
 }
 
+// Run the afore mentioned function on click
 button.addEventListener("click", () => {
   palindrome();
   userInput.value = "";
 });
 
+// Run the afore mentioned function on pressing enter
 userInput.addEventListener("keypress", function(event) {
   // If the user presses the "Enter" key on the keyboard
   if (event.key === "Enter") {
