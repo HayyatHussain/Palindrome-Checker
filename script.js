81f8223daf93a6679e8c971554b11702bf965a67
HayyatHussain <145599914+HayyatHussain@users.noreply.github.com>
2024-01-24 11:49:35 +0400

    Update script.js

--- script.js
+++ script.js
@@ -5,10 +5,15 @@ const output = document.getElementById("result");
 
 // Function to check if the user input is a palindrome
 const palindrome = () => {
+  // Regular Expression to check any input that is not a-z, A-Z and 0-9
   const regex = /[^a-zA-Z0-9]/g;
+  // Replace user input (special characters) with an empty string
   const filteredInput = userInput.value.replace(regex, "");
+  // Convert user input to lowercase for valid checking
   const lowerCaseStr = filteredInput.toLowerCase();
+  // Reverse user input
   const reverseStr = lowerCaseStr.split("").reverse().join("");
+  // Conditions to check user input validity
   if (lowerCaseStr.length == 0) {
     alert("Please input a value");
     return;
